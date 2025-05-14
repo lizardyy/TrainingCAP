25bb4923f80597354a0c0db2d74e492d41bb713b
Mahesa Lizardy <88304455+lizardyy@users.noreply.github.com>
2025-05-14 13:45:32 +0700

    Day 2 learning, Maven and Simple CAP Application

---
+++ DAY1/basic-java/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    int x = 5;
+
+    public static void main(String[] args) {
+        // Main myObj1 = new Main();
+        // Main myObj2 = new Main();
+        // System.out.println(myObj1.x);
+        // System.out.println(myObj2.x);
+
+        myMethod();
+        // myMethod();
+        // myMethod();
+
+    }
+
+    static void myMethod() {
+        System.out.println("I just got executed!");
+    }
+
+}
